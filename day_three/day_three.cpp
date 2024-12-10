8c90b1343c74e03fbeb0a5b3160fcc25fc7f22ea
oakinh <nikaohill@gmail.com>
2024-12-10 06:21:08 -0600

    Change day3 to run with toggle pattern

--- day_three/day_three.cpp
+++ day_three/day_three.cpp
@@ -12,17 +12,19 @@ int main() {
 
     std::string line;
     std::regex pattern("mul\\((\\d+),(\\d+)\\)");
+    std::regex patternWithToggle("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)");
 
     std::smatch match;
     int lineNumber = 0;
 
     int totalMul = 0;
 
+    bool yesDo = true;
+
     while (std::getline(file, line)) {
         lineNumber++;
-        // std::cout << "Line " << lineNumber << ": " << line << std::endl;
 
-        auto begin = std::sregex_iterator(line.begin(), line.end(), pattern);
+        auto begin = std::sregex_iterator(line.begin(), line.end(), patternWithToggle);
         auto end = std::sregex_iterator();
 
         if (begin == end) {
@@ -30,9 +32,19 @@ int main() {
         } else {
             for (std::sregex_iterator i = begin; i != end; i++) {
                 std::smatch match = *i;
-                int firstNum = std::stoi(match[1].str());
-                int secondNum = std::stoi(match[2].str());
-                totalMul += (firstNum * secondNum);
+                if (match[0].str() == "do()") {
+                    std::cout << "Do found, setting yesDo to true, before it was: " << yesDo << std::endl;
+                    yesDo = true;
+                } else if (match[0].str() == "don't()") {
+                    std::cout << "Don't found, setting yesDo to false, before it was: " << yesDo << std::endl;
+                    yesDo = false;
+                } else if (match.size() == 3 && yesDo) {
+                    int firstNum = std::stoi(match[1].str());
+                    int secondNum = std::stoi(match[2].str());
+                    totalMul += (firstNum * secondNum); 
+                } else {
+                    std::cout << "Mul encountered, yesDo is false, skipping: " << match[0] << std::endl;
+                }
             }
         }
     }
